dab5f3c8da6eb4ef56f7b0e0420b947ad4210e13
emgreen33 <greenemily33@gmail.com>
2016-01-23 13:25:02 -0800

    Updated problems

---
+++ SpecialPythagTriplet.rb
@@ -0,0 +1,29 @@
+# A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
+
+# a2 + b2 = c2
+# For example, 32 + 42 = 9 + 16 = 25 = 52.
+
+# There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+# Find the product abc.
+
+
+
+def pythag_triplet(num)
+	array = []
+	(1..num/2-2).each do |i|
+		(i..num/2-1).each do |j|
+			c = num - i - j
+			unless (c > j) && (i < j)
+				next
+			end
+
+			if (i ** 2 + j ** 2 == c ** 2)
+				array = [i, j, c]
+			end
+		end
+	end
+	p array
+end
+product = 1
+pythag_triplet(1000).each {|x| product *= x}
+puts "answer: #{product}"
