dec2ca6090f662d49fbc786785712aaf112c1067
emgreen33 <greenemily33@gmail.com>
2016-01-30 15:50:24 -0800

    Problem 12 commencing

--- Problem6.rb
+++ Problem6.rb
@@ -7,7 +7,6 @@
 # Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025 - 385 = 2640
 # Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
 
-
 def sum_of_square_nums(num)
 	result = 0
 	i = 1
--- Problem8.rb
+++ Problem8.rb
@@ -1,5 +1,6 @@
 #The four adjacent digits in the 1000-digit number that have the greatest product are 9 × 9 × 8 × 9 = 5832.
-#Find the thirteen adjacent digits in the 1000-digit number that have the greatest product. What is the value of this product?
+#Find the thirteen adjacent digits in the 1000-digit number that have the greatest product. 
+#What is the value of this product?
 
 def largest_prod(num)
 	a = num.split('')
